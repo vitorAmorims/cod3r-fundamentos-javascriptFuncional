87a3e936f365fc30e83a6e1b80c83b66a99b0775
vitorAmorims <vtamorims@gmail.com>
2020-10-18 14:47:12 -0300

    Resposta do desafio metodo map

---
+++ funcional/desafio_callback.js
@@ -0,0 +1,28 @@
+const carrinho = [
+    { nome: "Caneta", quantidade: 10, preco: 7.99 },
+    { nome: "Impressora", quantidade: 0, preco: 649.50 },
+    { nome: "Caderno", quantidade: 4, preco: 27.10 },
+    { nome: "Lapis", quantidade: 3, preco: 5.82 },
+    { nome: "Tesoura", quantidade: 1, preco: 19.20}
+]
+// desafio 1 - obter um array com os nomes dos produtos, utilize método map.
+const nomesDosProdutos = carrinho.map((produto) => {
+    return produto.nome;
+});
+// console.log(nomesDosProdutos);
+
+// refatorando
+
+const getNomes = produto => produto.nome;
+const nomesDosProdutos1 = carrinho.map(getNomes)
+console.log(nomesDosProdutos1);
+
+// desafio 2 - obter os valores consolidados de quantidade x preco, utilizando método map.
+const valoresConsolidados = carrinho.map(produto => produto.quantidade * produto.preco
+);
+// console.log(valoresConsolidados);
+
+// refatorando
+const getProdutosXPrecos = produto => produto.quantidade * produto.preco;
+const valoresConsolidados1 = carrinho.map(getProdutosXPrecos);
+console.log(valoresConsolidados1);
