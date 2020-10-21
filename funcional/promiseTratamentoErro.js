0f78958447c438b71e1623e541664688af02868f
vitorAmorims <vtamorims@gmail.com>
2020-10-21 10:30:08 -0300

    O nao tratamento de erro na Promise, pode quebrar codigo

--- funcional/promiseTratamentoErro.js
+++ funcional/promiseTratamentoErro.js
@@ -13,3 +13,5 @@ function funcionarOunao(valor, chanceErro, tempo) {
 funcionarOunao("testando...", 0.5, 4000)
 .then(console.log) //até aqui, o erro está sendo tratado pelo node
 .catch(error => console.log(`Erro: ${error}`)); // inserido método .cath para tratamento de erro na Promise.
+
+//eventualmente: Uma promise, sem o tratamento de erro pode causar quebra do código, e aplicação sair
