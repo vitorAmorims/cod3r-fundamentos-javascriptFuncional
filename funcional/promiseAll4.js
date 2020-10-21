438feaf3c0f0a3e223ddc157d7380a5714c5bc1f
vitorAmorims <vtamorims@gmail.com>
2020-10-21 08:48:13 -0300

    Promise.all, chamando as funçoes em sequencia

--- funcional/promiseAll4.js
+++ funcional/promiseAll4.js
@@ -10,3 +10,16 @@ function geradorDeNumeros(min,max,tempo) {
     });
 }
 // geradorDeNumeros(1,60,4000).then(console.log); chamando a função
+
+function gerarMatriz() {
+    console.log("Aguarde o retorno de Promise!")
+    return Promise.all([ //promise.all - consigo chamar a função de interese.
+        geradorDeNumeros(1,60,5000), 
+        geradorDeNumeros(1,60,1000),
+        geradorDeNumeros(1,60,500) //somente traz o resultado quando a Promise, estiver cumprida.
+    ])
+}
+// console.log(gerarMatriz()); output pending
+
+gerarMatriz().then(console.log);
+
