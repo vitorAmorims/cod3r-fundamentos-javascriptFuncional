644ad363ddafeb8fafb71e0eecac66bd25ed24c6
vitorAmorims <vtamorims@gmail.com>
2020-10-21 08:51:13 -0300

    Atribui o retorno de Promise.all, para uma constante

--- funcional/promiseAll4.js
+++ funcional/promiseAll4.js
@@ -13,11 +13,12 @@ function geradorDeNumeros(min,max,tempo) {
 
 function gerarMatriz() {
     console.log("Aguarde o retorno de Promise!")
-    return Promise.all([ //promise.all - consigo chamar a função de interese.
+    const meuArray = Promise.all([ //promise.all - consigo chamar a função de interese.
         geradorDeNumeros(1,60,5000), 
         geradorDeNumeros(1,60,1000),
         geradorDeNumeros(1,60,500) //somente traz o resultado quando a Promise, estiver cumprida.
     ])
+    return meuArray;
 }
 // console.log(gerarMatriz()); output pending
 
