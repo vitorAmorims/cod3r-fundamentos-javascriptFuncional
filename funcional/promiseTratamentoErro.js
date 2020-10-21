1ce6763424da5c2ecb7d86b744cc1ada91909d99
vitorAmorims <vtamorims@gmail.com>
2020-10-21 10:57:03 -0300

    tratamento de erro, pelo metodo .cath

--- funcional/promiseTratamentoErro.js
+++ funcional/promiseTratamentoErro.js
@@ -19,6 +19,6 @@ funcionarOunao("testando...", 0.5, 4000)
 .then(value => `Valor: ${value}`)
 .then(
     v => consol.log(v), 
-err => console.log(`Erro Esp: ${err}`) //neste caso, estou incluindo um tratamento antes do método cath. Consigo, saber em qual then aconteceu o erro!
+// err => console.log(`Erro Esp: ${err}`) //neste caso, estou incluindo um tratamento antes do método cath. Consigo, saber em qual then aconteceu o erro!
 )
-.catch(err => console.log(`Erro: ${err}`));
+.catch(err => console.log(`Erro geral: ${err}`));
